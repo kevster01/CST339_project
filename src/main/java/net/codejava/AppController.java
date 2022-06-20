bfa6778269c9758ee22f7c406103d538dae7a232
kevster01 <90598902+kevster01@users.noreply.github.com>
2022-06-19 22:15:36 -0700

    Add files via upload

---
+++ src/main/java/net/codejava/AppController.java
@@ -0,0 +1,23 @@
+package net.codejava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping("")
+	public String viewHomePage()
+	{
+		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model)
+	{
+		model.addAttribute("user", new User());
+		
+		return "signUp";
+	}
+}
