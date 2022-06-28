43fc1efd79f028e5d804df7a748cd65be284a0e0
kevster01 <90598902+kevster01@users.noreply.github.com>
2022-06-28 08:04:34 -0700

    Update AppController.java
    Adding processRegistration() which allows users to save and see the successful registration of a profile. 
    viewUsersList(), will allow users to view who else is in the system

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -1,8 +1,10 @@
 package net.codejava;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -19,5 +21,19 @@ public class AppController {
 		model.addAttribute("user", new User());
 		
 		return "signUp";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user)
+	{
+		repo.save(user);
+		
+		return "register_sucess";
+	}
+	
+	@GetMapping("/list_users")
+	public String viewUsersList()
+	{
+		return "users";
 	}
 }
